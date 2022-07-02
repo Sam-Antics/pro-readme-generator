38ea777f45199cc3cc418b1d61e7ca3a6a88bf2c
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-07-02 00:21:02 -0500

    created array of questions for user input

--- index.js
+++ index.js
@@ -1,15 +1,112 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+// requirements for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
+// const generateMd = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+ inquirer
+   .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your GitHub username? (Required)',
+      validate: userInput => {
+        if (userInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Please input the title for this project. (Required)',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('The README requires a project title.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a brief description for this project. (Required)',
+      validate: projectBrief => {
+        if (projectBrief) {
+          return true;
+        } else {
+          console.log('The README requires a brief description.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmInstall',
+      message: 'Does this project require installation by the user?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'Please providethe steps required to install your project.',
+      when: ({ confirmInstall }) => confirmInstall
+    },
+    {
+      type: 'confirm',
+      name: 'confirmUsage',
+      message: 'Do you have instructions for use of this project?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide instructions for use.',
+      when: ({ confirmUsage }) => confirmUsage
+    },
+    {
+      type: 'confirm',
+      name: 'confirmContribute',
+      message: 'Do you have instructions for potential contributors?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'Please provide contribution guidelines.',
+      when: ({ confirmContribute }) => confirmContribute
+    },
+    {
+      type: 'confirm',
+      name: 'confirmTest',
+      message: 'Do you have instructions for testing of this project?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Please provide instructions for testing.',
+      when: ({ confirmTest }) => confirmTest
+    },
+    {
+      type: 'checkbox',
+      name: 'license',
+      message: 'Which license is this application covered under?',
+      choices: ['MIT', 'GNU GPLv3', 'Mozilla', 'IBM', 'Apache 2.0', 'Unlicense'] 
+    }
+  ]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
 
 // Function call to initialize app
-init();
+// init(questions);
